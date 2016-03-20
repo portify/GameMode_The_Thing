492b79b605ba32483b4407bc8500904bc8fcad3c
Alex Noir <Varsash@Gmail.com>
2016-03-20 18:51:40 +0300

    ported over despair syndrome corpse persistance code

--- src/game.cs
+++ src/game.cs
@@ -1,115 +1,219 @@
+if (!isObject(GameRoundCleanup))
+	new SimSet(GameRoundCleanup);
 package TheThingGamePackage
 {
-  function MiniGameSO::reset(%this, %client)
-  {
-    Parent::reset(%this, %client);
+	function Armor::onDisabled(%this, %obj)
+	{
+		if (isObject(%obj.client))
+		{
+			%obj.client.stopViewingInventory();
+			if (%obj.client.inDefaultGame() && isObject(GameRoundCleanup))
+			{
+				%obj.inhibitRemoveBody = true;
+				GameRoundCleanup.add(%obj);
+			}
+		}
 
-    if (%this.owner != 0 || %this.lastResetTime != getSimTime())
-      return;
+		Parent::onDisabled(%this, %obj);
+	}
 
-    if (%this.numMembers == 0)
-      return;
+	function Player::removeBody(%this)
+	{
+		if (!%this.inhibitRemoveBody)
+			Parent::removeBody(%this);
+	}
 
-    %thingClient = %this.member[getRandom(%this.numMembers - 1)];
+	function GameConnection::onDeath(%client, %sourceObject, %sourceClient, %damageType, %damLoc)
+	{
+		if (%client.miniGame != $DefaultMiniGame)
+			return Parent::onDeath(%client, %sourceObject, %sourceClient, %damageType, %damLoc);
 
-    %thingPlayer = %thingClient.player;
-    %thingPlayer.changeDataBlock(TheThingData);
+		if (%sourceObject.sourceObject.isBot)
+		{
+			%sourceClientIsBot = 1;
+			%sourceClient = %sourceObject.sourceObject;
+		}
 
-    for (%i = 0; %i < %this.numMembers; %i++)
-    {
-      %member = %this.member[%i];
-      %player = %member.player;
+		%player = %client.player;
 
-      if (%member != %thingClient)
-      {
-        messageClient(%member, '', "\c5You are a human this round. Eliminate The Thing to win!");
-        // Give starter weapons here
+		if (isObject(%player))
+		{
+			%player.setShapeName("", 8564862);
+			if (isObject(%player.tempBrick))
+			{
+				%player.tempBrick.delete();
+				%player.tempBrick = 0;
+			}
+			%player.isBody = true;
+			%player.client = 0;
+		}
+		else
+			warn("WARNING: No player object in GameConnection::onDeath() for client '" @ %client @ "'");
 
-        %player.addTool(Colt1911Item);
-        %player.addTool(MagazineItem_45ACP_x7);
-        %player.addTool(MagazineItem_45ACP_x7);
-      }
-      else
-      {
-        messageClient(%member, '', "\c0You are be The Thing this round! Absorb all the humans to win!");
-        %player.addTool(Colt1911Item);
-      }
-    }
+		if (isObject(%client.camera) || isObject(%player))
+		{ // this part of the code isn't accurate
+			if (%client.getControlObject() != %client.camera)
+			{
+				%client.camera.setMode("Corpse", %player);
+				%client.camera.setControlObject(0);
+				%client.setControlObject(%client.camera);
+			}
+		}
 
-    %group = nameToID("BrickGroup_888888");
+		%client.player = 0;
+		%client.corpse = %player;
 
-    %maxItemPool = -1;
-    %maxAmmoPool = -1;
+		if ($Damage::Direct[$damageType] && getSimTime() - %player.lastDirectDamageTime < 100 && %player.lastDirectDamageType !$= "")
+			%damageType = %player.lastDirectDamageType;
 
-    %itemPool[%maxItemPool++] = "0";
-    %itemPool[%maxItemPool++] = "Colt1911Item";
-    %itemPool[%maxItemPool++] = "M1GarandItem";
-    %itemPool[%maxItemPool++] = "ColtWalkerItem";
-    %itemPool[%maxItemPool++] = "Remington870Item";
-    %itemPool[%maxItemPool++] = "RevolverItem";
-    %itemPool[%maxItemPool++] = "ThompsonItem";
-    %itemPool[%maxItemPool++] = "MicroUziItem";
+		if (%damageType == $DamageType::Impact && isObject(%player.lastPusher) && getSimTime() - %player.lastPushTime <= 1000)
+			%sourceClient = %player.lastPusher;
 
-    %ammoPool[%maxAmmoPool++] = "0";
-    %ammoPool[%maxAmmoPool++] = "MagazineItem_45ACP_x7";
-    %ammoPool[%maxAmmoPool++] = "Bullet357PackItem";
-    %ammoPool[%maxAmmoPool++] = "BulletBuckshotPackItem";
-    %ammoPool[%maxAmmoPool++] = "MagazineItem_45ACP_x20_SMG";
-    %ammoPool[%maxAmmoPool++] = "MagazineItem_3006_x8";
-    %ammoPool[%maxAmmoPool++] = "MagazineItem_MicroUzi";
-    %ammoPool[%maxAmmoPool++] = "MagazineItem_MicroUziExtended";
+		%message = '%2 killed %1';
 
-    for (%i = 0; %i < %group.NTObjectCount["_itemSpawn"]; %i++)
-    {
-      %brick = %group.NTObject["_itemSpawn", %i];
-      %brick.setItem(%itemPool[getRandom(%maxItemPool)]);
-    }
+		if (%sourceClient == %client || %sourceClient == 0)
+		{
+			%message = $DeathMessage_Suicide[%damageType];
+			%client.corpse.suicide = true;
+		}
+		else
+			%message = $DeathMessage_Murder[%damageType];
 
-    for (%i = 0; %i < %group.NTObjectCount["_ammoSpawn"]; %i++)
-    {
-      %brick = %group.NTObject["_ammoSpawn", %i];
-      %brick.setItem(%ammoPool[getRandom(%maxAmmoPool)]);
-    }
-  }
+		// removed mid-air kills code here
+		// removed mini-game kill points here
 
-  function MiniGameSO::checkLastManStanding(%this)
-  {
-    if (%this.owner != 0 || isEventPending(%this.resetSchedule))
-      return Parent::checkLastManStanding(%this);
+		%clientName = %client.getPlayerName();
 
-    %livingThing = false;
-    %livingHuman = false;
+		if (isObject(%sourceClient))
+			%sourceClientName = %sourceClient.getPlayerName();
+		else if (isObject(%sourceObject.sourceObject) && %sourceObject.sourceObject.getClassName() $= "AIPlayer")
+			%sourceClientName = %sourceObject.sourceObject.name;
+		else
+			%sourceClientName = "";
 
-    for (%i = 0; %i < %this.numMembers; %i++)
-    {
-      %member = %this.member[%i];
+		echo("\c4" SPC %sourceClientName SPC "killed" SPC %clientName);
+		// removed mini-game checks here
+		// removed death message print here
+		// removed %message and %sourceClientName arguments
+		messageClient(%client, 'MsgYourDeath', '', %clientName, '', '');//%client.miniGame.respawnTime);
 
-      if (isObject(%member.player))
-      {
-        if (%member.player.getDataBlock() == nameToID("TheThingData"))
-          %livingThing = true;
-        else
-          %livingHuman = true;
-      }
-    }
+		commandToClient(%client, 'CenterPrint', '', 1);
+		%client.miniGame.checkLastManStanding();
+	}
 
-    if (!%livingHuman)
-    {
-      if (!%livingThing)
-        %this.messageAll('', "\c5All humans have perished. The Thing may be gone, but it has still succeeded.");
-      else
-        %this.messageAll('', "\c5All humans have perished. The Thing has succeeded.");
-    }
-    else if (!%livingThing)
-      %this.messageAll('', "\c5The Thing has perished. The humans are successful.");
-    else
-      return 0;
+	function MiniGameSO::reset(%this, %client)
+	{
+		Parent::reset(%this, %client);
 
-    %this.messageAll('', "\c5Resetting the game in five seconds.");
-    %this.scheduleReset(5000);
+		if (%this.owner != 0 || %this.lastResetTime != getSimTime())
+			return;
 
-    return 0;
-  }
+		if (%this.numMembers == 0)
+			return;
+
+		if (isObject(GameRoundCleanup))
+			GameRoundCleanup.deleteAll();
+
+		%thingClient = %this.member[getRandom(%this.numMembers - 1)];
+
+		%thingPlayer = %thingClient.player;
+		%thingPlayer.changeDataBlock(TheThingData);
+
+		for (%i = 0; %i < %this.numMembers; %i++)
+		{
+			%member = %this.member[%i];
+			%player = %member.player;
+
+			if (%member != %thingClient)
+			{
+				messageClient(%member, '', "\c5You are a human this round. Eliminate The Thing to win!");
+				// Give starter weapons here
+
+				%player.addTool(Colt1911Item);
+				%player.addTool(MagazineItem_45ACP_x7);
+				%player.addTool(MagazineItem_45ACP_x7);
+			}
+			else
+			{
+				messageClient(%member, '', "\c0You are be The Thing this round! Absorb all the humans to win!");
+				%player.addTool(Colt1911Item);
+			}
+		}
+
+		%group = nameToID("BrickGroup_888888");
+
+		%maxItemPool = -1;
+		%maxAmmoPool = -1;
+
+		%itemPool[%maxItemPool++] = "0";
+		%itemPool[%maxItemPool++] = "Colt1911Item";
+		%itemPool[%maxItemPool++] = "M1GarandItem";
+		%itemPool[%maxItemPool++] = "ColtWalkerItem";
+		%itemPool[%maxItemPool++] = "Remington870Item";
+		%itemPool[%maxItemPool++] = "RevolverItem";
+		%itemPool[%maxItemPool++] = "ThompsonItem";
+		%itemPool[%maxItemPool++] = "MicroUziItem";
+
+		%ammoPool[%maxAmmoPool++] = "0";
+		%ammoPool[%maxAmmoPool++] = "MagazineItem_45ACP_x7";
+		%ammoPool[%maxAmmoPool++] = "Bullet357PackItem";
+		%ammoPool[%maxAmmoPool++] = "BulletBuckshotPackItem";
+		%ammoPool[%maxAmmoPool++] = "MagazineItem_45ACP_x20_SMG";
+		%ammoPool[%maxAmmoPool++] = "MagazineItem_3006_x8";
+		%ammoPool[%maxAmmoPool++] = "MagazineItem_MicroUzi";
+		%ammoPool[%maxAmmoPool++] = "MagazineItem_MicroUziExtended";
+
+		for (%i = 0; %i < %group.NTObjectCount["_itemSpawn"]; %i++)
+		{
+			%brick = %group.NTObject["_itemSpawn", %i];
+			%brick.setItem(%itemPool[getRandom(%maxItemPool)]);
+		}
+
+		for (%i = 0; %i < %group.NTObjectCount["_ammoSpawn"]; %i++)
+		{
+			%brick = %group.NTObject["_ammoSpawn", %i];
+			%brick.setItem(%ammoPool[getRandom(%maxAmmoPool)]);
+		}
+	}
+
+	function MiniGameSO::checkLastManStanding(%this)
+	{
+		if (%this.owner != 0 || isEventPending(%this.resetSchedule))
+			return Parent::checkLastManStanding(%this);
+
+		%livingThing = false;
+		%livingHuman = false;
+
+		for (%i = 0; %i < %this.numMembers; %i++)
+		{
+			%member = %this.member[%i];
+
+			if (isObject(%member.player))
+			{
+				if (%member.player.getDataBlock() == nameToID("TheThingData"))
+					%livingThing = true;
+				else
+					%livingHuman = true;
+			}
+		}
+
+		if (!%livingHuman)
+		{
+			if (!%livingThing)
+				%this.messageAll('', "\c5All humans have perished. The Thing may be gone, but it has still succeeded.");
+			else
+				%this.messageAll('', "\c5All humans have perished. The Thing has succeeded.");
+		}
+		else if (!%livingThing)
+			%this.messageAll('', "\c5The Thing has perished. The humans are successful.");
+		else
+			return 0;
+
+		%this.messageAll('', "\c5Resetting the game in five seconds.");
+		%this.scheduleReset(5000);
+
+		return 0;
+	}
 };
 
 activatePackage("TheThingGamePackage");
